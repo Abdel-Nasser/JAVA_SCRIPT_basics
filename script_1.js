7aeefc67f2b32c704dda19f4841c33602a00c84e
abdel nasser <abdel@nc-ass-vip.sdv.fr>
2021-02-25 18:45:53 +0100

    first commit

---
+++ script_1.js
@@ -0,0 +1,6 @@
+console.log("bonjour monde!")
+
+
+ name = prompt('taper votre votre prénom :');
+
+ console.log(`bonjour ${name}`);
---
+++ script_3.js
@@ -0,0 +1,15 @@
+
+ var etage = prompt('combien d étage voulez vous :');
+
+
+
+for(var i = 1; i <= etage; i++)
+
+{
+
+
+  console.log('#'.repeat(i));
+
+}
+
+
